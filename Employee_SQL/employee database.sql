1dbe83e77eabfba9a7ca6c0dc6083e5c7e9f5cbe
Haifa <haifanaj@gmail.com>
2020-12-25 12:23:50 -0600

    employee database update name of the columns

--- Employee_SQL/employee database.sql
+++ Employee_SQL/employee database.sql
@@ -1,21 +1,22 @@
 SELECT * FROM employees
 --List the following details of each employee: employee number, last name, first name, sex, and salary
-SELECT e.emp_no,e.last_name,e.first_name,e.sex,s.salary
+SELECT e.emp_no "Employee Number",e.last_name "Employee Last Number",e.first_name "Employee First Name",e.sex,s.salary
 FROM employees e
 INNER JOIN salaries s
 ON e.emp_no=s.emp_no;
 
 --List first name, last name, and hire date for employees who were hired in 1986
-SELECT first_name,last_name,hire_date
+SELECT first_name "Employee First Name",last_name "Employee Last Name",hire_date "Hire Date"
 FROM employees
-WHERE hire_date >= '1986-01-01' AND hire_date <= '1986-12-31'
+WHERE date_part('year',hire_date) = '1986'
 ORDER BY hire_date;
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name
-SELECT * FROM departments
-SELECT * FROM dept_manger 
+-- SELECT * FROM departments
+-- SELECT * FROM dept_manger 
 
-SELECT dm.dept_no,d.dept_name,dm.emp_no,e.last_name,e.first_name
+SELECT dm.dept_no  "Department Number",d.dept_name  "Department Name",dm.emp_no  "Manager Number", 
+e.last_name  "Manager Last Name", e.first_name  "Manager First Name"
 FROM dept_manger dm
 LEFT JOIN departments d ON dm.dept_no=d.dept_no
 LEFT JOIN employees e ON dm.emp_no = e.emp_no;
@@ -23,19 +24,19 @@ LEFT JOIN employees e ON dm.emp_no = e.emp_no;
 --List the department of each employee with the following information: employee number, last name, first name, and department name
 SELECT * FROM dept_emp
 
-SELECT de.emp_no,e.last_name,e.first_name,d.dept_name
+SELECT de.emp_no "Employee Number",e.last_name "Employee Last Name",e.first_name "Employee First Name",d.dept_name "Department Name"
 FROM dept_emp de
 LEFT JOIN employees e ON de.emp_no = e.emp_no
 LEFT JOIN departments d ON de.dept_no = d.dept_no;
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
-SELECT first_name,last_name,sex
+SELECT first_name "Employee First Name",last_name "Employee Last Name",sex "Sex"
 FROM employees
-WHERE first_name LIKE 'Hercules' AND last_name LIKE 'B%';
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name
-SELECT de.emp_no,e.last_name,e.first_name,d.dept_name
+SELECT de.emp_no "Employee Number",e.last_name "Employee Last Name",e.first_name "Employee First Name",d.dept_name "Department Name"
 FROM dept_emp de
 LEFT JOIN employees e ON de.emp_no = e.emp_no
 LEFT JOIN departments d ON de.dept_no = d.dept_no
@@ -43,14 +44,14 @@ WHERE d.dept_name = 'Sales';
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
 
-SELECT de.emp_no,e.last_name,e.first_name,d.dept_name
+SELECT de.emp_no "Employee Number",e.last_name "Employee Last Name",e.first_name "Employee First Name",d.dept_name "Department Name"
 FROM dept_emp de
 LEFT JOIN employees e ON de.emp_no = e.emp_no
 LEFT JOIN departments d ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
-SELECT last_name , COUNT(last_name)
+SELECT last_name "Employee Last Name", COUNT(last_name) "Frequency Last Name"
 FROM employees
 GROUP BY last_name
 ORDER BY last_name DESC;
